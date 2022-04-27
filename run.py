6bc39cf7b97356f4a0a1ec9757c8ad5a348e926f
Cairoden Macalanggan <82497374+cairoden15@users.noreply.github.com>
2022-04-27 05:46:35 -0400

    Add initial files

---
+++ run.py
@@ -0,0 +1,4 @@
+from OnlineBookStore import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,3 @@
+# Postgresql Database URI
+
+DB_URI = 'postgresql://postgres:root@localhost:5432/onlinebooking'
