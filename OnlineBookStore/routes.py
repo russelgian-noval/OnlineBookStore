f5595d12ab001f402981be0fa67942dc08bb9a09
Cairoden Macalanggan <82497374+cairoden15@users.noreply.github.com>
2022-06-11 09:22:59 -0400

    added home page, contact, and about routes

--- OnlineBookStore/routes.py
+++ OnlineBookStore/routes.py
@@ -6,3 +6,24 @@ from OnlineBookStore import app, db, bcrypt
 from OnlineBookStore.forms import RegistrationForm, LoginForm, UpdateAccountForm, AdminLoginForm, AddBookForm, UpdateBookForm
 from OnlineBookStore.models import User,Admin,Book,Cart,Order,OrderBook
 from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return render_template('public/index.html')
+
+# homepage
+@app.route("/home")
+@login_required
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact Page')
