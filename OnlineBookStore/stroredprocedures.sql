cd467f4492b8139e23e37926a504ce5c8fcef08e
Khalid-Rangaig <80198667+Khalid-Rangaig@users.noreply.github.com>
2022-06-12 17:47:03 +0800

    checkout storedprocedure added

--- OnlineBookStore/stroredprocedures.sql
+++ OnlineBookStore/stroredprocedures.sql
@@ -31,3 +31,35 @@ begin
     );
 end;
 $$;
+
+-- to checkout book
+CREATE OR REPLACE FUNCTION GET_CART(par_uid int) RETURNS json
+    LANGUAGE plpgsql
+    AS $$
+declare
+    loc_prod_record record;
+    loc_prod_json json[];
+    loc_size int default 0;
+begin
+    for loc_prod_record in 
+    select cart.id, book.image_file, book.title, book.author, book.price from book 
+    JOIN cart ON book.id = cart.book_id where cart.user_id = par_uid 
+    loop
+        loc_prod_json = loc_prod_json ||
+                        json_build_object(
+                            'id', loc_prod_record.id,
+                            'image_file', loc_prod_record.image_file,
+                            'title', loc_prod_record.title,
+                            'author', loc_prod_record.author,
+                            'price', loc_prod_record.price
+                        );
+        loc_size = loc_size + 1;
+    end loop;
+    
+    return json_build_object(
+        'status', 'OK',
+        'size', loc_size,
+        'cart', loc_prod_json
+    );
+end;
+$$;
